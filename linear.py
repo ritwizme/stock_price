ccbafb5285394d84014c79cec4f05cbba4c35412
Ritwiz kaustoob <ritwizme@gmail.com>
2018-05-03 18:03:08 +1100

    Add files via upload

--- linear.py
+++ linear.py
@@ -1,35 +1,59 @@
-import pandas as pd
-import quandl
-import math
+import quandl, math
 import numpy as np
+import pandas as pd
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
 
+df = quandl.get("WIKI/GOOGL")
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
-quandl.ApiConfig.api_key = 'dNKsx_NzCgQZAkusTZKG'
-df=quandl.get_table('WIKI/PRICES',paginate=True)
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
 
-df  = df[['adj_open','adj_high','adj_low','adj_close','adj_volume']]
-df['hl_pct']=(df['adj_high']-df['adj_low'])/df['adj_close']*100.0
-df['pct_change']=(df['adj_close']-df['adj_open'])/df['adj_open']*100.0
-df = df[['adj_close','hl_pct','pct_change','adj_volume']]
-
-forcast_col='adj_close'
-df.fillna(-99999, inplace=True)
-
-forcast_out=int(math.ceil(0.01*len(df)))
-print(forcast_out)
-df['label']=df[forcast_col].shift(-forcast_out)
-df.dropna(inplace=True)
-
-
-X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
+X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-X_train, X_test,y_train,y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
 
-clf = LinearRegression()
-clf.fit(X_train,y_train)
-accuracy = clf.score(X_test,y_test)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, confidence, forecast_out)
+
+
+
+df['Forcast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+
+df['Adj. Close'].plot()
+df['Forcast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
+
